408f01b993289c628e22212052116873cf7f9466
angel <96trinidad@gmail.com>
2022-07-21 20:01:37 -0400

    first commit

---
+++ src/pages/Login.js
@@ -0,0 +1,11 @@
+import React from "react";
+
+const LoginPage = () => {
+  return (
+    <div className="container p-4">
+      <h1>Login Page</h1>
+    </div>
+  );
+};
+
+export default LoginPage;
